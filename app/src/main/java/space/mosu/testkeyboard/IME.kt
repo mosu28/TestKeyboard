87cde8a02ef2e81e0ea430f7cc687eaa6cf07411
tibimosu <tibimosu1128@gmail.com>
2019-09-17 22:59:33 +0900

    fix

--- app/src/main/java/space/mosu/testkeyboard/IME.kt
+++ app/src/main/java/space/mosu/testkeyboard/IME.kt
@@ -10,17 +10,17 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import kotlin.math.*
 
-enum class JpCharacters(val chars: String) {
-    A("あいうえお"),
-    K("かきくけこ"),
-    S("さしすせそ"),
-    T("たちつてと"),
-    N("なにぬねの"),
-    H("はひふへほ"),
-    M("まみむめも"),
-    Y("や（ゆ）よ"),
-    R("らりるれろ"),
-    W("わをんー")
+enum class JpCharacters(val chars: List<String>) {
+    A(listOf("あいうえお", "ぁぃぅぇぉ")),
+    K(listOf("かきくけこ", "がぎぐげご")),
+    S(listOf("さしすせそ", "ざじずぜぞ")),
+    T(listOf("たちつてと", "だぢづでど")),
+    N(listOf("なにぬねの")),
+    H(listOf("はひふへほ", "ばびぶべぼ", "ぱぴぷぺぽ")),
+    M(listOf("まみむめも")),
+    Y(listOf("や（ゆ）よ", "ゃ ゅ ょ")),
+    R(listOf("らりるれろ")),
+    W(listOf("わをんー"))
 }
 
 class IME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
@@ -92,6 +92,10 @@ class IME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
     override fun onKey(primaryCode: Int, codes: IntArray?) {
         val ic = currentInputConnection
         if (isCharacter(primaryCode)) {
+            if (!isPrediction) {
+                isPrediction = true
+                currentCharacters = ""
+            }
             currentCharacters += getCharacter(primaryCode)
             ic.setComposingText(currentCharacters, 1)
 //            ic.finishComposingText()
@@ -102,18 +106,25 @@ class IME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 //                    currentCharacters =
 //                        currentCharacters.slice(0 until currentCursor - 1) +
 //                                currentCharacters.slice(currentCursor until currentCharacters.length)
-//                    ic.deleteSurroundingText(1, 0)
-                    ic.setComposingText(currentCharacters, 1)
+                    if (isPrediction) {
+                        currentCharacters = currentCharacters.slice(0 until currentCharacters.length - 1)
+                        ic.setComposingText(currentCharacters, 1)
+                    } else {
+                        ic.deleteSurroundingText(1, 0)
+                    }
                 }
                 KeyEvent.KEYCODE_ENTER -> {
-                    ic.finishComposingText()
-                    currentCharacters = ""
-                    ic.sendKeyEvent(
-                        KeyEvent(
-                            KeyEvent.ACTION_DOWN,
-                            KeyEvent.KEYCODE_ENTER
+                    if (isPrediction) {
+                        ic.finishComposingText()
+                        isPrediction = false
+                    } else {
+                        ic.sendKeyEvent(
+                            KeyEvent(
+                                KeyEvent.ACTION_DOWN,
+                                KeyEvent.KEYCODE_ENTER
+                            )
                         )
-                    )
+                    }
                 }
                 KeyEvent.KEYCODE_DPAD_LEFT -> {
                     ic.sendKeyEvent(
@@ -122,7 +133,6 @@ class IME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
                             KeyEvent.KEYCODE_DPAD_LEFT
                         )
                     )
-                    currentCursor = max(0, currentCursor - 1)
                 }
                 KeyEvent.KEYCODE_DPAD_RIGHT -> {
                     ic.sendKeyEvent(
@@ -131,7 +141,19 @@ class IME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
                             KeyEvent.KEYCODE_DPAD_RIGHT
                         )
                     )
-                    currentCursor = min(currentCharacters.length, currentCursor + 1)
+                }
+                KeyEvent.KEYCODE_11 -> {
+                    if (isPrediction) {
+                        if (currentCharacters.isNotBlank()) {
+                            val last = getLastCharacters(currentCharacters.last())
+                            if (last == ' ') return
+                            currentCharacters = currentCharacters.slice(0 until currentCharacters.length - 1)
+                            currentCharacters += last
+                            ic.setComposingText(currentCharacters, 1)
+                        }
+                    } else {
+
+                    }
                 }
                 else -> {
                 }
@@ -166,22 +188,49 @@ class IME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 //        }
     }
 
+    private fun getLastCharacters(char: Char): Char = when {
+        char in JpCharacters.A.chars.joinToString("") -> nextCharacter(char, JpCharacters.A.chars)
+        char in JpCharacters.K.chars.joinToString("") -> nextCharacter(char, JpCharacters.K.chars)
+        char in JpCharacters.S.chars.joinToString("") -> nextCharacter(char, JpCharacters.S.chars)
+        char in JpCharacters.T.chars.joinToString("") -> nextCharacter(char, JpCharacters.T.chars)
+        char in JpCharacters.N.chars.joinToString("") -> nextCharacter(char, JpCharacters.N.chars)
+        char in JpCharacters.H.chars.joinToString("") -> nextCharacter(char, JpCharacters.H.chars)
+        char in JpCharacters.M.chars.joinToString("") -> nextCharacter(char, JpCharacters.M.chars)
+        char in JpCharacters.Y.chars.joinToString("") -> nextCharacter(char, JpCharacters.Y.chars)
+        char in JpCharacters.R.chars.joinToString("") -> nextCharacter(char, JpCharacters.R.chars)
+        char in JpCharacters.W.chars.joinToString("") -> nextCharacter(char, JpCharacters.W.chars)
+        else -> ' '
+    }
+
+    private fun nextCharacter(char: Char, strs: List<String>): Char {
+        for (i in strs.indices) {
+            if (char in strs[i]) {
+                val next = if (i + 1 >= strs.size) 0 else i + 1
+                for (j in strs[i].indices) {
+                    if (char == strs[i][j])
+                    return strs[next][j]
+                }
+            }
+        }
+        return ' '
+    }
+
     override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
 
         return super.onKeyUp(keyCode, event)
     }
 
     private fun getCharacter(primaryCode: Int): String = when (primaryCode) {
-            KeyEvent.KEYCODE_1 -> setCharacter(JpCharacters.A.chars)
-            KeyEvent.KEYCODE_2 -> setCharacter(JpCharacters.K.chars)
-            KeyEvent.KEYCODE_3 -> setCharacter(JpCharacters.S.chars)
-            KeyEvent.KEYCODE_4 -> setCharacter(JpCharacters.T.chars)
-            KeyEvent.KEYCODE_5 -> setCharacter(JpCharacters.N.chars)
-            KeyEvent.KEYCODE_6 -> setCharacter(JpCharacters.H.chars)
-            KeyEvent.KEYCODE_7 -> setCharacter(JpCharacters.M.chars)
-            KeyEvent.KEYCODE_8 -> setCharacter(JpCharacters.Y.chars)
-            KeyEvent.KEYCODE_9 -> setCharacter(JpCharacters.R.chars)
-            KeyEvent.KEYCODE_0 -> setCharacter(JpCharacters.W.chars)
+            KeyEvent.KEYCODE_1 -> setCharacter(JpCharacters.A.chars[0])
+            KeyEvent.KEYCODE_2 -> setCharacter(JpCharacters.K.chars[0])
+            KeyEvent.KEYCODE_3 -> setCharacter(JpCharacters.S.chars[0])
+            KeyEvent.KEYCODE_4 -> setCharacter(JpCharacters.T.chars[0])
+            KeyEvent.KEYCODE_5 -> setCharacter(JpCharacters.N.chars[0])
+            KeyEvent.KEYCODE_6 -> setCharacter(JpCharacters.H.chars[0])
+            KeyEvent.KEYCODE_7 -> setCharacter(JpCharacters.M.chars[0])
+            KeyEvent.KEYCODE_8 -> setCharacter(JpCharacters.Y.chars[0])
+            KeyEvent.KEYCODE_9 -> setCharacter(JpCharacters.R.chars[0])
+            KeyEvent.KEYCODE_0 -> setCharacter(JpCharacters.W.chars[0])
             else -> ""
         }
 
